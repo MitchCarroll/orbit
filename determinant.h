b1e2b16b3c0807415efb95008b42f2f18a172196
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2012-05-05 12:54:23 -0700

    Fixed determinant function.

--- determinant.h
+++ determinant.h
@@ -1,22 +1,26 @@
 #ifndef DETERMINANT_H
 #define DETERMINANT_H 1
 
-double * determinant(double **matrix)
+double * determinant(double matrix[5][5])
 {
   double temp[10];
-  double result[5];
+  for(int t=0;t<10;t++) temp[t]=1.0;
+  double *result = new double[5];
   for(int c=0;c<5;c++)
     for(int x=0;x<5;x++) {
-      y=(x+c)%5;
+      int y=(x+c)%5;
       temp[c]*=matrix[x][y];
     }
   for(int c=4;c>=0;c--)
     for(int x=0;x<5;x++) {
-      y=(c-x)%5;
-      temp[5+c]*=matrix[x][y];
+      int y=(c-x);
+      if(y<0) y+=5;
+      temp[9-c]*=matrix[x][y];
     }
-  for(int x=0;x<5;x++)
-    result[x]=temp[x]-temp[x+5];
+  for(int x=0;x<5;x++) {
+    result[x]=temp[x]-temp[9-x];
+  }
   return result;
+}
 
 #endif
